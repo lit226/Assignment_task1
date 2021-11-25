60e71e5641db0ae73546386f6102edad737ddec3
Ayush Rawat <56024169+lit226@users.noreply.github.com>
2021-11-25 12:51:00 +0530

    Add files via upload

---
+++ task-1_solution/demo.py
@@ -0,0 +1,25 @@
+from util import Visualize_images
+data_path='C:\\Users\\ar590\\ML_&_DL_Project\\Assignment\\Data Visualization\\data\\1.json'
+img_path = 'C:\\Users\\ar590\\ML_&_DL_Project\\Assignment\\Data Visualization\\images\\1.jpg'
+
+Visualize_images(img_path,data_path)
+
+data_path='C:\\Users\\ar590\\ML_&_DL_Project\\Assignment\\Data Visualization\\data\\2.json'
+img_path = 'C:\\Users\\ar590\\ML_&_DL_Project\\Assignment\\Data Visualization\\images\\2.jpg'
+
+Visualize_images(img_path,data_path)
+
+data_path='C:\\Users\\ar590\\ML_&_DL_Project\\Assignment\\Data Visualization\\data\\3.json'
+img_path = 'C:\\Users\\ar590\\ML_&_DL_Project\\Assignment\\Data Visualization\\images\\3.jpg'
+
+Visualize_images(img_path,data_path)
+
+data_path='C:\\Users\\ar590\\ML_&_DL_Project\\Assignment\\Data Visualization\\data\\4.json'
+img_path = 'C:\\Users\\ar590\\ML_&_DL_Project\\Assignment\\Data Visualization\\images\\4.jpg'
+
+Visualize_images(img_path,data_path)
+
+data_path='C:\\Users\\ar590\\ML_&_DL_Project\\Assignment\\Data Visualization\\data\\5.json'
+img_path = 'C:\\Users\\ar590\\ML_&_DL_Project\\Assignment\\Data Visualization\\images\\5.jpg'
+
+Visualize_images(img_path,data_path)
